2b13d297651e775e3b49f2aa2f2eed4f641736dc
Timduru <timduru@gmail.com>
2012-12-10 20:51:55 +0100

    Add back custom recovery command support
    (use in BoardConfig.mk)
    Credits: Donald Munn
    
    Change-Id: Ie6687120594caa9c437827344d2ffd2e1e6065f4
    
    Conflicts:
    	libcutils/android_reboot.c

--- libcutils/android_reboot.c
+++ libcutils/android_reboot.c
@@ -20,6 +20,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <cutils/android_reboot.h>
@@ -118,6 +119,14 @@ int android_reboot(int cmd, int flags, char *arg)
             break;
 
         case ANDROID_RB_RESTART2:
+#ifdef RECOVERY_PRE_COMMAND
+            if (!strcmp(arg,"recovery")) {
+                char cmd[PATH_MAX];
+                sprintf(cmd, RECOVERY_PRE_COMMAND " %s", arg);
+                system(cmd);
+            }
+#endif
+
             ret = __reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
                            LINUX_REBOOT_CMD_RESTART2, arg);
             break;
